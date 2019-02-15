960b1605458df665f5188aceced55fb80c54aae1
PetrNovak96 <novp19@vse.cz>
2019-02-15 20:09:45 +0100

    další úprava deklarace CPT

--- assets/tinymce/ea-tinymce.js
+++ assets/tinymce/ea-tinymce.js
@@ -2,9 +2,11 @@ jQuery('#ea-tlacitko').click(function(){
     zobrazReporty();
 });
 
-$( document ).delegate( "#ea-button-vlozit", "click", function() {
-    var content = getShortCodeContent();
-    tinyMCE.activeEditor.execCommand('mceInsertContent', 0,
-        content);
-    tb_remove();
+jQuery(document).ready(function($) {
+    $( document ).delegate( "#ea-button-vlozit", "click", function() {
+        var content = getShortCodeContent();
+        tinyMCE.activeEditor.execCommand('mceInsertContent', 0,
+            content);
+        tb_remove();
+    });
 });
--- src/EasyMiner_Integration/EasyminerReportType.php
+++ src/EasyMiner_Integration/EasyminerReportType.php
@@ -12,8 +12,38 @@ class EasyminerReportType
     public function zaregistruj_typ()
     {
         $args = array(
-            'supports' => array('title', 'custom-fields')
+            'name'                => 'Analytické zprávy',
+            'singular_name'       => 'Analytická zpráva',
+            'labels' => array(
+                'name'=> 'Analytické zprávy',
+                'singular_name'=> 'Analytická zpráva',
+                'menu_name'=> 'Analytické zprávy',
+                'all_items'=> 'Všechny analytické zprávy',
+                'view_item'=> 'Zobrazit',
+                'search_items'=> 'Hledat',
+                'not_found'=>__('Not found'),
+                'not_found_in_trash'=>__('Not found in Trash'),
+            ),
+            'description'         => '',
+            'supports'            => array('title', 'custom-fields'),
+            'taxonomies'          => [],
+            'hierarchical'        => false,
+            'public'              => false,
+            'show_ui'             => true,
+            'show_in_menu'        => true,
+            'show_in_admin_bar'   => true,
+            'menu_position'       => 5,
+            'menu_icon'           => 'dashicons-analytics',
+            'can_export'          => true,
+            'has_archive'         => true,
+            'capabilities'        => array(
+                'create_posts' => 'do_not_allow'
+            ),
+            'map_meta_cap' => true,
+            'rewrite'             => array(
+                'slug'              => 'easyminer_reports'
+            )
         );
-        register_post_type('easyminer_report', $args);
+        register_post_type('easyminer-report', $args);
     }
 }
