de7cbc0fde4bf6f6dc070ce4849eb590207cf457
PetrNovak96 <novp19@vse.cz>
2019-02-16 09:58:25 +0100

    Row action je jenom smazat a po kliku na report se otevře view, ne edit

--- src/EasyMiner_Integration/EasyminerReportType.php
+++ src/EasyMiner_Integration/EasyminerReportType.php
@@ -4,9 +4,59 @@ namespace EasyMiner_Integration;
 
 class EasyminerReportType
 {
+    private $typeName;
+    private $customColumnName;
+
     public function __construct()
     {
+        $this->typeName = 'easyminer-report';
+        $this->customColumnName = 'nahled';
         add_action('init', array($this, 'zaregistruj_typ'));
+        add_filter('post_row_actions', array($this, 'report_row_actions'), 10, 2);
+        add_filter("bulk_actions-edit-$this->typeName", array($this, 'report_bulk_actions'));
+        add_filter("manage_edit-$this->typeName"."_columns",
+            array($this, 'manage_columns'));
+        add_action("manage_$this->typeName"."_posts_custom_column",
+            array($this, 'custom_column'), 10, 2);
+    }
+
+    public function manage_columns($columns) {
+        unset($columns['title']);
+        $columns[$this->customColumnName] = 'Title';
+        $columns = array(
+            'cb' => $columns['cb'],
+            'nahled' => $columns[$this->customColumnName],
+            'date' => $columns['date']
+        );
+        return $columns;
+    }
+
+    public function custom_column($column_name, $post_id) {
+        if ($column_name != $this->customColumnName)
+            return;
+        $post = get_post($post_id);
+        ?>
+        <strong>
+            <a class="row-title"
+               href="<?php echo get_permalink($post) ?>"
+               aria-label="<?php echo get_the_title($post) ?>">
+                <?php echo get_the_title($post) ?>
+            </a>
+        </strong>
+        <div class="row-actions">
+            <span class="trash">
+                <a class="submitdelete"
+                   href="<?php echo get_delete_post_link($post)?>">
+                    <?php echo _('Trash');?>
+                </a>
+            </span>
+        </div>
+        <?php
+    }
+
+    public function report_bulk_actions($actions) {
+        unset($actions['edit']);
+        return $actions;
     }
 
     public function zaregistruj_typ()
@@ -36,14 +86,20 @@ class EasyminerReportType
             'menu_icon'           => 'dashicons-analytics',
             'can_export'          => true,
             'has_archive'         => true,
-            'capabilities'        => array(
-                'create_posts' => 'do_not_allow'
+            'capabilities' => array(
+                'create_posts' => 'do_not_allow',
             ),
             'map_meta_cap' => true,
             'rewrite'             => array(
                 'slug'              => 'easyminer_reports'
             )
         );
-        register_post_type('easyminer-report', $args);
+        register_post_type($this->typeName, $args);
+//        wp_insert_post(array(
+//            'post_title' => 'Název',
+//            'post_content' => 'Obsah',
+//            'post_status' => 'publish',
+//            'post_type' => $this->typeName,
+//        ), false);
     }
 }
