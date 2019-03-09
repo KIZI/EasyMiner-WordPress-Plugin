89fedc72702c4ffd8ee9c59d6a2d3984905e655b
PetrNovak96 <novp19@vse.cz>
2019-03-09 09:23:01 +0100

    XMLReader umí číst XML i z řetězce

--- xml_reader_snippet/xmlreader_snippet.php
+++ xml_reader_snippet/xmlreader_snippet.php
@@ -1,7 +1,21 @@
 <?php
 
 $xml = new XMLReader();
-$xml->open('soubor.xml');
+$xml->XML('<notes>
+    <note id="1">
+        <to>Tove</to>
+        <from>Jani</from>
+        <heading>Reminder</heading>
+        <body>Don\'t forget me this weekend!</body>
+    </note>
+    <note id="2">
+        <to>Petr</to>
+        <from>Dan</from>
+        <heading>DULEZITE</heading>
+        <body>Dojed Dom!</body>
+    </note>
+</notes>', 'UTF-8');
+//$xml->open('soubor.xml');
 
 $notes = array();
 
