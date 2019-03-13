c40afbd07afd7a0ccd6a0984b31a552f52ba6535
PetrNovak96 <novp19@vse.cz>
2019-03-13 20:55:31 +0100

    Vhozena kompaktní verze transformace

--- themes/single-easyminer-report.php
+++ themes/single-easyminer-report.php
@@ -1,19 +1,15 @@
 <?php
-/*
-Template Name: Single EasyMiner Report
-*/
 
-$post = get_post();
 
-get_header();
-?>
-<div style="text-align: center">
-    <h1 class="title"><?php echo $post->post_title; ?></h1>
-    <div>
-        <?php echo $post->post_content; ?>
-    </div>
-    <!--- BUĎ SE TA TRANSFORMACE BUDE ODEHRÁVAT TADY,
-    NEBO SE ZAVOLÁ OBJEKT, KTERÝ TU TRANSFORMACI UDĚLÁ -->
-</div>
-<?php
-get_footer();
+
+$xslDoc = new DOMDocument();
+$xslDoc->load(__DIR__."/4FTPMML2HTML.xsl");
+
+$xmlDoc = new DOMDocument();
+$xmlDoc->load(__DIR__."/LM1.xml");
+
+$proc = new XSLTProcessor();
+$proc->importStylesheet($xslDoc);
+echo $proc->transformToXml($xmlDoc);
+
+//get_footer();
