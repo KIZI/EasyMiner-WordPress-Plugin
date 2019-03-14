5b38a92e07e7868b674da416a34dcb1615b5fb48
PetrNovak96 <novp19@vse.cz>
2019-03-14 18:58:57 +0100

    Vhozena kompaktní verze transformace

--- themes/single-easyminer-report.php
+++ themes/single-easyminer-report.php
@@ -1,9 +1,9 @@
 <?php
-
-
+//header("Content-type: application/xhtml+xml");
+//get_header();
 
 $xslDoc = new DOMDocument();
-$xslDoc->load(__DIR__."/4FTPMML2HTML.xsl");
+$xslDoc->load(__DIR__."/4FTPMML2HTML.xsl", LIBXML_NOCDATA);
 
 $xmlDoc = new DOMDocument();
 $xmlDoc->load(__DIR__."/LM1.xml");
