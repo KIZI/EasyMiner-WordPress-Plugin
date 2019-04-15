9f6f92ef942bb3099ce555820a0d6d119bd50a75
PetrNovak96 <novp19@vse.cz>
2019-04-15 13:31:06 +0200

    Umím vkládat i v HTML módu

--- assets/tinymce/ea-tinymce.js
+++ assets/tinymce/ea-tinymce.js
@@ -4,8 +4,18 @@ jQuery(document).ready(function($) {
 
     $( document ).delegate( "#ea-button-insert", "click", function() {
         var content = getReportContent();
-        tinyMCE.activeEditor.execCommand('mceInsertContent', 0,
-            content);
+        var editor = tinyMCE.activeEditor;
+        if(editor) {
+            tinyMCE.activeEditor.execCommand('mceInsertRawHTML', 0,
+                content);
+        } else {
+            var textarea = $('#content');
+            var cursorPosition = textarea.prop("selectionStart");
+            var text = textarea.val();
+            var textBefore = text.substring(0,  cursorPosition);
+            var textAfter  = text.substring(cursorPosition, text.length);
+            textarea.val(textBefore + content + textAfter);
+        }
         tb_remove();
     });
 });
